2906acdb3c0b378fe79c96d2bf5bfeebee25e68a
Jnikem <154122338+jnikem@users.noreply.github.com>
2026-03-26 17:41:58 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+string S;
+
+int alpha[26] = {0, };
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+
+    cin.tie(0);
+
+		cin >> S;
+
+	for(char c : S) {
+
+		alpha[c - 'a'] += 1;
+
+	}
+
+	
+
+	for(int i : alpha) {
+
+		cout << i << " ";
+
+	}
+
+    return 0;
+
+}
